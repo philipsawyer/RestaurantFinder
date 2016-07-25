845075539fb4d86eff1af7badf904183026591b4
Philip Sawyer <philsawyer94@gmail.com>
2016-07-25 11:36:01 -0400

    Initial Commit

---
+++ RestaurantFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestaurantFinder
+//
+//  Created by Philip Sawyer on 7/25/16.
+//  Copyright © 2016 Philip Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
