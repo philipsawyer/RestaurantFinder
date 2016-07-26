5ac215d1d9cf77464d8af0aee8b89bda85ee0ea8
Philip Sawyer <philsawyer94@gmail.com>
2016-07-26 10:13:27 -0400

    Initial commit

---
+++ RestaurantFinder/RestaurantDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantDetailViewController.swift
+//  RestaurantFinder
+//
+//  Created by Philip Sawyer on 7/26/16.
+//  Copyright © 2016 Philip Sawyer. All rights reserved.
+//
+
+import Cocoa
+
+class RestaurantDetailViewController: UIViewController {
+
+}
