1eca4fde6c9c2740141f40cb1cbd7c5e0864ccd0
Philip Sawyer <philsawyer94@gmail.com>
2016-07-26 11:23:14 -0400

    Added test cases for tablevc and detailvc

--- RestaurantFinderTests/RestaurantFinderTests.swift
+++ RestaurantFinderTests/RestaurantFinderTests.swift
@@ -17,8 +17,9 @@ class RestaurantFinderTests: FBSnapshotTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        recordMode = false
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
@@ -29,13 +30,19 @@ class RestaurantFinderTests: FBSnapshotTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testRestaurantsTableView() {
+    func testRestaurantsList() {
+        let brazos = YLPBusiness()
+        brazos.name = "Brazos Tacos"
+        brazos.snippetText = "Authentic Texas breakfast tacos - highly recommend for those who've been to Austin! We were blown away by the \"I Love You so Much\" taco."
 
-        recordMode = true
-        let temp = YLPBusiness()
-        temp.name = "Brazos Tacos"
-        temp.snippetText = "Authentic Texas breakfast tacos - highly recommend for those who've been to Austin! We were blown away by the \"I Love You so Much\" taco."
-        let state = AppState(restaurants: [temp], currentRestaurant: temp)
+
+        let bebedero = YLPBusiness()
+        bebedero.name = "The Bebedero"
+        bebedero.snippetText = "A great new spot downtown with a cute little patio, too! We were a bit blinded by the sun, and our waitress helped us maneuver our umbrella so we were blind..."
+        let state = AppState(restaurants: [brazos, bebedero], currentRestaurant: brazos)
+
+        //incorrect state will cause error
+        //let state = AppState(restaurants: [brazos], currentRestaurant: brazos)
 
         let window: UIWindow!
 
@@ -48,5 +55,26 @@ class RestaurantFinderTests: FBSnapshotTestCase {
 
         FBSnapshotVerifyView(window)
     }
-    
+
+
+    func testRestaurantDetail() {
+        let brazos = YLPBusiness()
+        brazos.name = "Brazos Tacos"
+        brazos.snippetText = "Authentic Texas breakfast tacos - highly recommend for those who've been to Austin! We were blown away by the \"I Love You so Much\" taco."
+        //brazos.snippetText = "Here is an incorrect state"
+
+
+        let state = AppState(restaurants: [], currentRestaurant: brazos)
+
+        let window: UIWindow!
+
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("RestaurantDetailViewController") as! RestaurantDetailViewController
+        UIApplication.sharedApplication().keyWindow?.rootViewController = UINavigationController(rootViewController: controller)
+        controller.loadViewIfNeeded()
+        window = UIApplication.sharedApplication().keyWindow
+
+        controller.newState(state)
+
+        FBSnapshotVerifyView(window)
+    }
 }
